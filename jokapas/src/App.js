d7c04959069cba6c231567fc3904272b1b106664
Pántya Barnabás <pantya.barnabas@gmail.com>
2023-10-29 22:40:44 +0100

    Navbar hozzáadása

--- jokapas/src/App.js
+++ jokapas/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from 'react-router'
+import { HomePage } from './pages/Home/HomePage';
+import { Navbar } from './components/Navbar/Navbar';
 function App() {
   return (
     <Routes>
-      <Route path='*'/>
+      <Route path='*' element={
+        <>
+          <Navbar/>
+          <HomePage/>
+        </>}/>
     </Routes>
   );
 }
---
+++ jokapas/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './navbar.css'
+
+export function Navbar() {
+    return(
+        <div className="container">
+            <nav className='navbar'>
+            
+            </nav>
+        </div>
+        
+    )
+}
