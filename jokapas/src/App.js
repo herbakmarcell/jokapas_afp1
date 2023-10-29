807cc75f5523ac1fed7ef5871a901ecd50de2444
Pántya Barnabás <pantya.barnabas@gmail.com>
2023-10-29 22:08:06 +0100

    Frontend project létrehozása

---
+++ jokapas/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {Route, Routes} from 'react-router'
+function App() {
+  return (
+    <Routes>
+      <Route path='*'/>
+    </Routes>
+  );
+}
+
+export default App;
