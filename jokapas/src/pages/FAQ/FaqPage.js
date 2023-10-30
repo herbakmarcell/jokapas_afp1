34277c4d9a2147b7dbe366b0246a4cc368a1d597
Pántya Barnabás <pantya.barnabas@gmail.com>
2023-10-30 21:47:00 +0100

    GYIK kérdések és válaszok function

--- jokapas/src/pages/FAQ/FaqPage.js
+++ jokapas/src/pages/FAQ/FaqPage.js
@@ -7,16 +7,11 @@ export function FaqPage(){
                 <h1 className='main-title'>Gyakori Kérdések</h1>
                 <div className='faq-text'>
                     <Faq question={"Hol találom a megvásárolható termékeket?"} answer={"A navigációs sávon (az oldal tetején található) kattintson a Kínálat gombra."}/>
-                    <h2 className='cim'>Hogyan tudom megkezdeni a vásárlást?</h2>
-                    <p className='text'>Jelentkezzen be a navigációs sávon található Bejelentkezés gombbal.</p>
-                    <h2 className='cim'>Hogyan tudok bejelentkezni?</h2>
-                    <p className='text'>A navigációs sávon (az oldal tetején található) kattintson a Bejelentkezés gombra</p>
-                    <h2 className='cim'>Problémám akadt a rendeléssel kapcsolatban.</h2>
-                    <p className='text'>Kattintson az Ügyintézés gombra.</p>
-                    <h2 className='cim'>Hol tudom követni a rendelésemet?</h2>
-                    <p className='text'>Kattintson a Rendeléskövetés gombra a navigációs sávon.</p>
-                    <h2 className='cim'>Hogyan tudok saját terméket eladni?</h2>
-                    <p className='text'>Kattintson az Árusítás gombra a navigációs sávon</p>
+                    <Faq question={"Hogyan tudom megkezdeni a vásárlást?"} answer={"Jelentkezzen be a navigációs sávon található Bejelentkezés gombbal."}/>
+                    <Faq question={"Hogyan tudok bejelentkezni?"} answer={"A navigációs sávon (az oldal tetején található) kattintson a Bejelentkezés gombra."}/>
+                    <Faq question={"Problémám akadt a rendeléssel kapcsolatban."} answer={"Kattintson az Ügyintézés gombra."}/>
+                    <Faq question={"Hol tudom követni a rendelésemet?"} answer={"Kattintson a Rendeléskövetés gombra a profilján belül."}/>
+                    <Faq question={"Hogyan tudok saját terméket eladni?"} answer={"Kattintson az Árusítás gombra a navigációs sávon."}/>
                 </div>
             </div>
         </div>
