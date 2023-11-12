93be21ca5c1c1512776dd55608e8c91cd061d6da
herbakmarcell <herbakmarcell@gmail.com>
2023-11-12 19:04:23 +0100

    Regisztráció hozzáadása

--- jokapas_backend/src/app.module.ts
+++ jokapas_backend/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from "./user.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'marcell',
+      password: '',
+      database: 'jokapas',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+        secret: 'secret',
+        signOptions: {expiresIn: '1d'}
+    })
+  ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
