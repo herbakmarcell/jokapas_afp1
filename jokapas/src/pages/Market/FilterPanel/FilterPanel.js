84714f3b469ab1e9356883322642dacb62292ac6
Pántya Barnabás <pantya.barnabas@gmail.com>
2023-11-17 23:22:40 +0100

    Filter szűrő function hozzáaádsa

---
+++ jokapas/src/pages/Market/FilterPanel/FilterPanel.js
@@ -0,0 +1,6 @@
+import "./filterPanel.css"
+export function FilterPanel() {
+    return (
+        <div className="filter-container"></div>
+    )
+}
--- jokapas/src/pages/Market/MarketPage.js
+++ jokapas/src/pages/Market/MarketPage.js
@@ -1,8 +1,13 @@
+
+import { FilterPanel } from './FilterPanel/FilterPanel'
 import './marketPage.css'
 export function MarketPage(){
     return(
         <div className="market-container">
             <div className="market-title">Kínálat</div>
+            <div className="market-content-container">
+                <FilterPanel/>
+            </div>
         </div>
     )
 }
