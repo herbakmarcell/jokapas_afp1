f9d35169f28fc181d25d744a5f2c73d469fae91c
Pántya Barnabás <pantya.barnabas@gmail.com>
2023-11-19 00:38:25 +0100

    Termék információ szerkesztése

--- jokapas/src/pages/SingleProduct/SingleProduct.js
+++ jokapas/src/pages/SingleProduct/SingleProduct.js
@@ -1,7 +1,29 @@
+import { RatingStars } from "../Market/ProductList/RatingStars";
+import "./singleProduct.css"
 import { useParams } from "react-router-dom";
 export function SingleProduct(){
     const {id} = useParams();
-    return(<div>
-        Termék id-je: {id}
-    </div>)
+    return(
+        <div className="single-product-container">
+            <div className="product-title-container">
+                Termék címe
+            </div>
+            <div className="product-content-container">
+                <div className="product-container-div">
+                    <div className="product-img-container">
+                        <img className="product-img" src="shovel.png" alt="product-img"/>
+                        </div>
+                        
+                    <div className="product-details">Részletek</div>
+                </div>
+                <div className="user-div">
+                    <img className="profile-img" src="avatar.jpg" alt="profile-img"/>
+                    <div className="profile-data">
+                        <span>Teszt Elek (teszt)</span>
+                        <div><RatingStars rating={3}/></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
