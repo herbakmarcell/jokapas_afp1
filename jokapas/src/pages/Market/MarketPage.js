8e4bf5fdfd0ad7c822a04a3c0f420bf77606ec1e
Pántya Barnabás <pantya.barnabas@gmail.com>
2023-11-25 00:05:34 +0100

    Spinner létrehozása & marketPage-hez adása

--- jokapas/src/pages/Market/MarketPage.js
+++ jokapas/src/pages/Market/MarketPage.js
@@ -1,8 +1,15 @@
 
+import { useState } from 'react'
 import { FilterPanel } from './FilterPanel/FilterPanel'
 import { ProductList } from './ProductList/ProductList'
 import './marketPage.css'
+import { Spinner } from '../../components/Spinner/Spinner'
 export function MarketPage(){
+    const [isLoading, setLoading] = useState(false)
+
+    if(isLoading){
+        return <Spinner/>
+    }
     return(
         <div className="market-container">
             <div className="market-title">Kínálat</div>
