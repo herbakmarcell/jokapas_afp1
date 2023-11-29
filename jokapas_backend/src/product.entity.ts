352ceca51504a664636dc30919c7793463fd1a3a
FlexesFarkas <detarilevi@gmail.com>
2023-11-29 12:55:59 +0100

    termék entitás létrehozva

---
+++ jokapas_backend/src/product.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('products')
+export class Product{
+    @PrimaryGeneratedColumn()
+    product_id: number;
+    @Column()
+    name: string;
+    @Column()
+    desc: string;
+    @Column()
+    tag: string;
+    
+
+}
