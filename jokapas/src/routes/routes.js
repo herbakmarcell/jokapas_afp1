cf1bedf12ce0d64f1c9abbe37033d07fc726480c
FourthRuffle <fodor.gyozo.benedek@gmail.com>
2023-12-06 13:10:51 +0100

    Kosár hozzáadva

--- jokapas/src/routes/routes.js
+++ jokapas/src/routes/routes.js
@@ -5,7 +5,7 @@ import { AdminPage } from "../pages/Administration/AdministrationPage";
 import { FaqPage } from "../pages/FAQ/FaqPage";
 import { ProfilePage } from "../pages/Profile/ProfilePage";
 import { RegisterPage } from "../pages/Register/RegisterPage";
-import { CartPage } from "../Cart/CartPage";
+import { CartPage } from "../pages/Cart/CartPage";
 import { AdminSuccess } from "../pages/Administration/AdminSuccess";
 import { SingleProduct } from "../pages/SingleProduct/SingleProduct";
 
@@ -16,11 +16,12 @@ export const publicRoutes = [
     {path: "faq", page: FaqPage},
     {path: "/", page: HomePage},
     {path: "register", page: RegisterPage},
-    {path: "product/:id", page:SingleProduct}   
+    {path: "product/:id", page:SingleProduct},
+    {path: "cart", page: CartPage},
+
 ]
 
 export const protectedRoutes = [
     {path: "profile", page: ProfilePage},
-    {path: "cart", page: CartPage},
     {path: "adminsuccess", page: AdminSuccess},
 ]
