7ac6573fc4e64fafe14e5dc211de939fb04ddc16
FlexesFarkas <detarilevi@gmail.com>
2023-12-09 11:54:33 +0100

    product tag és desc lekérdezések létrehozva

--- jokapas_backend/src/app.controller.ts
+++ jokapas_backend/src/app.controller.ts
@@ -108,5 +108,48 @@ export class AppController {
             throw new UnauthorizedException();
         }
     }
-    
+
+
+
+    @Get('productdesc')
+    async productdesc(@Req() request: Request) {
+        try {
+            const cookie = request.cookies['jwt'];
+
+            const data = await this.jwtService.verifyAsync(cookie);
+
+            if (!data) {
+                throw new UnauthorizedException();
+            }
+
+            const products = await this.appService.findProduct({product_id: data['product_id']});
+            const productdesc = products.description;
+            return productdesc;
+
+            
+        } catch (e) {
+            throw new UnauthorizedException();
+        }
+    }
+
+    @Get('producttag')
+    async producttag(@Req() request: Request) {
+        try {
+            const cookie = request.cookies['jwt'];
+
+            const data = await this.jwtService.verifyAsync(cookie);
+
+            if (!data) {
+                throw new UnauthorizedException();
+            }
+
+            const products = await this.appService.findProduct({product_id: data['product_id']});
+            const producttag = products.tag;
+            return producttag;
+
+            
+        } catch (e) {
+            throw new UnauthorizedException();
+        }
+    }
 }
